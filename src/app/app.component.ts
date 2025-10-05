3c055ddf829ec9ed207f30a2e4cccc48235589a7
Jhuliani <jhulianisantos@gmail.com>
2025-10-05 09:08:15 -0300

    ajuste menu principal, ajuste login e cadastro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CPaineladminComponent } from "./_pages/c-paineladmin/c-paineladmin.component";
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CPaineladminComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputTextModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Aureus-Bank';
+  showLayout = true;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe((e: NavigationEnd) => {
+        const url = e.urlAfterRedirects || e.url;
+        this.showLayout = !(
+          url.startsWith('/login') ||
+          url.startsWith('/cadastro')
+        );
+      });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,37 @@
 import { Routes } from '@angular/router';
-import { CCadastroComponent } from './_pages/c-cadastro/c-cadastro.component';
+
+// Páginas "públicas" (sem sidebar)
 import { CLoginComponent } from './_pages/c-login/c-login.component';
-import { CPainelclienteComponent } from './_pages/c-painelcliente/c-painelcliente.component';
+import { CCadastroComponent } from './_pages/c-cadastro/c-cadastro.component';
+
+// Páginas "internas" (com sidebar)
+import { CBemvindoComponent } from './_pages/c-bemvindo/c-bemvindo.component';
 import { CMeuscontratosComponent } from './_pages/c-meuscontratos/c-meuscontratos.component';
 import { CPaineladminComponent } from './_pages/c-paineladmin/c-paineladmin.component';
-import { CBemvindoComponent } from './_pages/c-bemvindo/c-bemvindo.component';
 import { CConsultarComponent } from './_pages/c-consultar/c-consultar.component';
 import { CSimulacaoComponent } from './_pages/_simulacao/c-simulacao/c-simulacao.component';
-import { CSolicitacaoComponent } from '../app/_pages/_solicitacao/c-solicitacao/c-solicitacao.component';
+import { CSolicitacaoComponent } from './_pages/_solicitacao/c-solicitacao/c-solicitacao.component';
+import { CPainelclienteComponent } from './_pages/c-painelcliente/c-painelcliente.component';
+import { CSettingsComponent } from './_pages/c-settings/c-settings.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'login', pathMatch: 'full'},
-    {path: 'cadastro', component: CCadastroComponent},
-    {path: 'login', component: CLoginComponent},
-    {path: 'painelcliente',component: CPainelclienteComponent},
-    {path: 'meuscontratos', component: CMeuscontratosComponent},
-    {path: 'painel', component: CPaineladminComponent},
-    {path: 'bemvindo', component: CBemvindoComponent},
-    {path: 'consultar', component: CConsultarComponent},
-    {path: 'simulacao', component: CSimulacaoComponent},
-    {path: 'solicitacao', component: CSolicitacaoComponent}
+  // Redireciona raiz para login
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  // Rotas públicas (fora do layout principal)
+  { path: 'login', component: CLoginComponent },
+  { path: 'cadastro', component: CCadastroComponent },
+
+  // Rotas internas (usadas pelo layout global já presente no AppComponent)
+  { path: 'inicio', component: CBemvindoComponent },
+  { path: 'contratos', component: CMeuscontratosComponent },
+  { path: 'painelcliente', component: CPainelclienteComponent },
+  { path: 'painel', component: CPaineladminComponent },
+  { path: 'consultar', component: CConsultarComponent },
+  { path: 'simulacao', component: CSimulacaoComponent },
+  { path: 'solicitacao', component: CSolicitacaoComponent },
+  { path: 'settings', component: CSettingsComponent },
+
+  // Rota curinga (404 opcional)
+  { path: '**', redirectTo: 'login' },
 ];
