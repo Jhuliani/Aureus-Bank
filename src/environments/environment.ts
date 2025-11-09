ef002090e6506c43bcd97dc47c154d10811d0e30
Jhuliani <jhulianisantos@gmail.com>
2025-11-09 13:15:37 -0300

    url api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8000',
+  apiUrl: 'https://api-aureus.onrender.com',
   viaCepUrl: 'https://viacep.com.br/ws',
   fipeApiUrl: 'https://parallelum.com.br/fipe/api/v1'
 };
